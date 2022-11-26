26a77524f911b2048d4e1438623da8cdc36bb3d3
jasdeep panum <jasdeep.panum@kcl.ac.uk>
2022-11-26 04:54:22 +0000

    add name text field to model

--- things/models.py
+++ things/models.py
@@ -3,5 +3,6 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class Thing(AbstractUser):
+    name = models.TextField()
     description = models.TextField()
     quantity = models.IntegerField()

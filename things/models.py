a507cec1d9452a496f7e479c075c30af0d1eee61
jasdeep panum <jasdeep.panum@kcl.ac.uk>
2022-12-02 13:15:19 +0000

    update migrations

--- things/models.py
+++ things/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-class Thing():
+class Thing(models.Model):
     name = models.TextField()
-    description = models.TextField()
+    description = models.TextField(default='0000000')
     quantity = models.IntegerField()

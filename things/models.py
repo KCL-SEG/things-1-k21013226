bc8623b8e27b4e69bcdbaeca4f5fad899e812b95
jasdeep panum <jasdeep.panum@kcl.ac.uk>
2022-12-02 13:48:24 +0000

    Add model refinements

--- things/models.py
+++ things/models.py
@@ -1,8 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 class Thing(models.Model):
-    name = models.TextField()
-    description = models.TextField(default='0000000')
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=120,blank=True)
+    quantity = models.PositiveIntegerField(validators=[MinValueValidator(0),
+                                       MaxValueValidator(100)])
+
+
+
+#name  must not be blank,
+
+#description need not be unique,
+
+#quantity need not be unique, and must be an integer value between 0 and 100 (inclusive).
+#quantity may be 0 and it may be 100, but not less than 0 and not more than 100.

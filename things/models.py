f2561a7be862ab6aee2cbd38783080b3a6218a6f
jasdeep panum <jasdeep.panum@kcl.ac.uk>
2022-12-02 13:04:03 +0000

    Update Model

--- things/models.py
+++ things/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-class Thing(AbstractUser):
+class Thing():
     name = models.TextField()
     description = models.TextField()
     quantity = models.IntegerField()
